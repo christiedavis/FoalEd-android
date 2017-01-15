53ceb4b5edcc0f55573ffc478a0b21a2e7277f28
Brendan <brendan.reetz@gmail.com>
2017-01-15 16:52:47 +1300

    Database is working (clearing all records on close though?) Camera selection is working, but not photo select from the gallery

---
+++ app/src/main/java/com/abc/foaled/DatabaseManager.java
@@ -0,0 +1,8 @@
+package com.abc.foaled;
+
+/**
+ * Created by PC on 15/01/2017.
+ */
+
+public class DatabaseManager {
+}
---
+++ app/src/main/java/com/abc/foaled/DatePickerFragment.java
@@ -0,0 +1,83 @@
+package com.abc.foaled;
+
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.widget.DatePicker;
+import android.app.DialogFragment;
+import android.app.Dialog;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    TextView textBox;
+
+    /**
+     * This is used to provide the destination of the value chosen
+     * @param view The Text View the chosen date is to be put into
+     */
+    public void setViewResult (TextView view) {
+        textBox = view;
+    }
+
+    /**
+     * If there is already a date set in the Text View, parse the string
+     * and pass the relevant values in the DatePickerDialog creation. Or
+     * else just use today's date values
+     *
+     * @param savedInstanceState The bundle of data shared by the creating activity
+     * @return The DatePickerDialog to show the user
+     */
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        int year, month, day;
+        Calendar c = Calendar.getInstance();
+        if (textBox != null) {
+            String date = textBox.getText().toString();
+
+            if (date.equals("")) {
+                year = c.get(Calendar.YEAR);
+                month = c.get(Calendar.MONTH);
+                day = c.get(Calendar.DAY_OF_MONTH);
+            } else {
+                String[] dateArray = date.split("/");
+
+                year = Integer.parseInt(dateArray[2]);
+                month = Integer.parseInt(dateArray[1]) - 1;
+                day = Integer.parseInt(dateArray[0]);
+            }
+
+            //Create and return a new instance of TimePickerDialog
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+        else {
+            year = c.get(Calendar.YEAR);
+            month = c.get(Calendar.MONTH);
+            day = c.get(Calendar.DAY_OF_MONTH);
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+    }
+
+    /**
+     * Sets the Date of Birth TextView to the value specified in the
+     * DateTimePicker Dialog Fragment. This is the onTimeSet() callback
+     * method, and gets called when the user chooses the date
+     *
+     * @param view The DatePicker view that was being interacted with
+     * @param year The year selected
+     * @param month The month selected
+     * @param day The day selected
+     */
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        if (textBox != null)
+            textBox.setText(day+"/"+ (month+1)+"/"+year);
+    }
+}
