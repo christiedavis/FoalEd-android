62d0cf9992351ae6f4e4521ebd1456b84861f303
Brendan <brendan.reetz@gmail.com>
2017-02-07 22:04:56 +1300

    Added the Notification Scheduler

--- app/src/main/java/com/abc/foaled/Activity/AddNewHorseActivity.java
+++ app/src/main/java/com/abc/foaled/Activity/AddNewHorseActivity.java
@@ -22,8 +22,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
---
+++ app/src/main/java/com/abc/foaled/Milestones.java
@@ -0,0 +1,12 @@
+package com.abc.foaled;
+
+/**
+ * Created by Brendan on 7/02/2017.
+ */
+
+public class Milestones {
+
+
+
+    public Milestones() {}
+}
