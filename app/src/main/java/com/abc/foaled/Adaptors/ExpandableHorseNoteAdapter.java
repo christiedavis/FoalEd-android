2dd46fa99952461407da9b7438bfde840a966e58
Steven Brown <steven.brown@xero.com>
2017-04-18 20:51:15 +1200

    Added some dumb stuff

---
+++ app/src/main/java/com/abc/foaled/Adaptors/ExpandableHorseNoteAdapter.java
@@ -0,0 +1,10 @@
+package com.abc.foaled.Adaptors;
+
+import android.widget.BaseExpandableListAdapter;
+
+/**
+ * Created by steven.brown on 18/04/2017.
+ */
+
+public class ExpandableHorseNoteAdapter extends BaseExpandableListAdapter {
+}
